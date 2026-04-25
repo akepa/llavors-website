4a7d60b35f09ef4b66a0d6f35a600bb3f8c78008
Adam <mirth88@gmail.com>
2026-04-26 00:01:46 +0200

    fix: add trailing slash to base path so images resolve correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://akepa.github.io/llavors-website',
-  base: '/llavors-website',
+  base: '/llavors-website/',
   output: 'static',
   i18n: {
     defaultLocale: 'ca',
